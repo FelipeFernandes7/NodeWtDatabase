a7ccad85f3698271f32629adaa53a926ccc9d2ba
felipe <felipekurosaki123@gmail.com>
2022-05-06 11:06:50 -0300

    salvando pra não dar erro no projeto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const express = require("express");
+const { listenerCount } = require("process");
+
+const app = express();
+
+app.listen(3000, () => console.log("server started http://localhost:3000"));
 
 const connection = mysql.createConnection({
-    host:'devstart.ca8wsggzuflh.us-east-1.rds.amazonaws.com',
-    user:'admin',
-    database:'DevStart_Luiz',
-    waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: "devstart.ca8wsggzuflh.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  database: "DevStart_Luiz",
+  password:"S2[W}vG$3P#!b%]Z)Pa}A{2+G",
+
 });
 
 connection.query(
-    'SELECT * FROM `tb_empregado` WHERE `nome_emp`= "cargo"',
-    function(err, results, fields){
-        console.log(results);
-    }
-)
+  'SELECT * FROM `tb_empregado` ',
+  function (err, results, fields) {
+    console.log(results);
+  }
+);
